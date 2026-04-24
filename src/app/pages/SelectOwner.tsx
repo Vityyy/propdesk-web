bc1a1be93e22aba4c143a550a304f0bd98017ae8
Giuse-04 <gmancinelli@fi.uba.ar>
2026-04-24 17:06:40 -0300

    Change in dependency: react-router-dom -> react-router

--- src/app/pages/SelectOwner.tsx
+++ src/app/pages/SelectOwner.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 import authService from '../../services/authService';
 import { useOwner } from '../context/OwnerContext';
 
